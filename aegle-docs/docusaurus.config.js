2a97e2075923dff89492a4072bab2e0fb7b94f30
kuang-da <kuangda@seas.upenn.edu>
2025-04-11 00:47:33 -0400

    updated tab name of the documentation

--- aegle-docs/docusaurus.config.js
+++ aegle-docs/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'PhenoCycler-PennTMC Pipeline',
+  title: 'Aegle: PhenoCycler Pipeline @ PennTMC',
   tagline: '2D Segmentation for PhenoCycler',
   favicon: 'img/favicon.ico',
   // staticDirectories: ['static'],
